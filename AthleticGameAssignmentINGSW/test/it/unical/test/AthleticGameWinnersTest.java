e6260932e1c4ff22ca4488b248185583aa648b43
my <my@160.97.252.106>
2017-11-18 11:08:30 +0100

    last change

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameWinnersTest.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameWinnersTest.java
@@ -1,5 +1,8 @@
 package it.unical.test;
+import java.time.Instant;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -25,10 +28,18 @@ public class AthleticGameWinnersTest {
 	}
 	@Test
 	public void getWinnerTest_1() {
-		
+		athleticGame.addArrival("Roumia", Instant.now().plusMillis(1000));
+		athleticGame.addArrival("Mohamed", Instant.now().plusMillis(2000));
+		athleticGame.addArrival("Youseef", Instant.now().plusMillis(3000));
+		athleticGame.addArrival("Ahmed", Instant.now().plusMillis(4000));
+		athleticGame.addArrival("Elmetwally", Instant.now().plusMillis(5000));
+
+		Assert.assertEquals("Roumia", athleticGame.getWinner());
 	}
 	@Test
 	public void getWinnerTest_2() {
+		athleticGame.addArrival(" ", Instant.now().plusMillis(1000));
 		
+		Assert.assertEquals(" ", athleticGame.getWinner());
 	}
 }
